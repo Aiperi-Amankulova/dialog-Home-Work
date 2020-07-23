6058e44626ff01628dcb8539d5c387f2522e2f52
Aiperi <aip.amankulova@gmail.com>
2020-07-24 01:19:36 +0600

    Adding a new dialogue

--- app/src/main/java/com/example/dialog/MainActivity.kt
+++ app/src/main/java/com/example/dialog/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.dialog
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextThemeWrapper
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import java.lang.System.exit
 import java.util.*
 
@@ -17,33 +19,45 @@ class MainActivity : AppCompatActivity() {
     private var etBirthday : EditText? = null
     private var btnSave : Button? = null
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupViews()
         setupListeners()
+        datePickerDialog()
+        alertDialog()
     }
-
-
-
     private fun setupViews() {
         etFirstName = findViewById(R.id.etFirstName)
         etLastName = findViewById(R.id.etLastName)
         etBirthday = findViewById(R.id.etBirthday)
         btnSave = findViewById(R.id.btnSave)
     }
+    @RequiresApi(Build.VERSION_CODES.N)
     private fun setupListeners() {
         btnSave?.setOnClickListener {
-            createAlertDialog()
+            alertDialog()
+        }
+        etBirthday?.setOnClickListener {
+            datePickerDialog()
         }
     }
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun  datePickerDialog() {
+        val dataDialog = DatePickerDialog(this)
+        dataDialog.setOnDateSetListener { view, year, month, dayOfMonth ->
+            etBirthday?.setText(getString(R.string.date, dayOfMonth, month, year))
 
-    private fun createAlertDialog() {
+        }
+        dataDialog.show()
+    }
+
+    private fun alertDialog() {
         AlertDialog.Builder(ContextThemeWrapper(this, R.style.attention ))
             .setTitle(getString(R.string.keep))
             .setPositiveButton(getString(R.string.yes)) { dialog, which ->
-
             }
             .setNegativeButton(getString(R.string.no)) { dialog, which ->
             }
